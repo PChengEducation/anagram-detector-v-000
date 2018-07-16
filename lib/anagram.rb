c4a54c8d60aa0d4c73cadd691c91a2689139c005
Pan Cheng <pchengeducation@hotmail.com>
2018-07-16 01:52:16 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    word_array = []
+    @word.each do |letter|
+      word_array << letter
+    end
+    word_array.sort_by{|k| k["value"]}
+     array.each do |match_word|
+
+  end
+
+end #end of the Anagram class
